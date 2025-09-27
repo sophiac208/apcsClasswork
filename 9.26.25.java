7a1b930130ab88cba49a1d0925e3edf5b5f52e8c
Sophia Chen <schen73@stuy.edu>
2025-09-27 16:21:46 -0400

    completed the cw code for 9.26

--- 9.26.25.java
+++ 9.26.25.java
@@ -1,1 +1,26 @@
 
+public class ArrayTest{
+public static void main(String[]args){
+  //1 initialize the array
+  //int[] anArray = ???
+  int[] anArray = new int[11];
+
+  //2 print the array variable
+  System.out.print("part 2: "+ anArray);
+
+  //Part 3 Write the loop to initialize it
+  for (int i = 0; i < anArray.length; i++) {
+    anArray[i] = 100 + (i *10);
+  }
+
+  //Part 4: write a single println statement to print :
+  //        anArray[0], anArray[5], and anArray[10]
+  System.out.print("part 4: " + anArray[0]+ anArray[5] + anArray[10]);
+
+  //Part 5 Write a loop to print the whole array on one line
+  System.out.print("part 5: ");
+  for (int i = 0; i < anArray.length; i++) {
+    System.out.print(anArray[i]+ " ");
+  }
+}
+}
