9c1326259bc1b1d76024492f666e46ed130a03bd
Sophia Chen <schen73@stuy.edu>
2025-09-30 11:44:29 -0400

    Added test cases for returnCopy

---
+++ 9.30.25/MyArrays.java
@@ -0,0 +1,22 @@
+public class MyArrays( {
+  public static int[] returnCopy(int[]ary) {
+    for (int i = 0; i < ary.length; i++) {
+
+    }
+  }
+
+
+
+//test cases
+  public static void (String[] args){
+    int[] ary = {1,2,3,4};
+    int[] cpy = {1,2,3,4};
+    System.out.print("array 1:" + ary + "array 2:" + cpy);
+    int[] ary = {2,2,3,4};
+    int[] cpy = {1,2,3,4};
+    System.out.print("array 1:" + ary + "array 2:" + cpy);
+    int[] ary = {};
+    int[] cpy = {1,2,3,4};
+    System.out.print("array 1:" + ary + "array 2:" + cpy);
+  }
+}
